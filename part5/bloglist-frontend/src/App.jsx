195996c6a2d3a3f9c1842536849848bd7469af99
mariellegustafsson <marielle.gust@gmail.com>
2024-12-12 11:42:32 +0200

    adding notes via frontend works

--- part5/bloglist-frontend/src/App.jsx
+++ part5/bloglist-frontend/src/App.jsx
@@ -37,6 +37,7 @@ const App = () => {
       window.localStorage.setItem(
         'loggedBlogAppUser', JSON.stringify(user)
       ) 
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
