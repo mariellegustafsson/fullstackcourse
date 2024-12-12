d2a0a140d95e691192d9b05bc282d21b088c6f90
mariellegustafsson <marielle.gust@gmail.com>
2024-12-12 12:17:05 +0200

    5.19

--- part5/playwright_testing/tests/blog_app.spec.js
+++ part5/playwright_testing/tests/blog_app.spec.js
@@ -1,3 +1,5 @@
+
+const { loginWith, createBlog } = require('./helper')
 const { test, expect, beforeEach, describe } = require('@playwright/test')
 
 describe('Blog app', () => {
@@ -24,24 +26,50 @@ describe('Blog app', () => {
         })
 
   test('login works', async ({ page }) => {
-
-    await page.getByRole('button', { name: 'login' }).click()
-    await page.getByRole('textbox').first().fill('marielle')
-    await page.getByRole('textbox').last().fill('secret')
-    await page.getByRole('button', { name: 'login' }).click()
-  
-  
+    const username = 'marielle'
+    const password = 'secret'
+    await loginWith(page, username, password)
     await expect(page.getByText('marielle is logged in')).toBeVisible()
 
   })
   test('login fails with wrong credentials', async ({ page }) => {
-    await page.getByRole('button', { name: 'login' }).click()
-    await page.getByRole('textbox').first().fill('marielle')
-    await page.getByRole('textbox').last().fill('wrong')
-    await page.getByRole('button', { name: 'login' }).click()
+    await loginWith(page, 'marielle', 'wrong')
 // I expect that the user is not logged in if the log in page is still visible after trying to login
     await expect(page.getByText('Log in to application')).toBeVisible()
   })
-    
+
+  describe('when logged in', () => {
+    beforeEach(async ({ page }) => {
+        await loginWith(page, 'marielle', 'secret')
+    })
+
+
+test('a new blog can be created', async ({ page }) => {
+    const content = {
+        title: 'this weeks menu',
+        author: 'simon',
+        url: 'menu.com'
+    }
+    await createBlog(page, content)
+
+    await expect(page.getByText(`${content.title} ${content.author}`)).toBeVisible();
+  }) // problem: det blir jwt tokenerror
+
+  
+  describe('and a note exists', () => {
+    beforeEach(async ({ page }) => {
+        content = {
+            title: 'my day',
+            author: 'rebecca',
+            url: 'life.com'
+        }
+        await createBlog(page, content)
+    })
+
+    //test('a blogpost can be liked', async ({ page }) => { })
+
+  }) 
+})
 
 })
+ 
---
+++ part5/playwright_testing/tests/helper.js
@@ -0,0 +1,15 @@
+const loginWith = async (page, username, password)  => {
+    await page.getByRole('textbox').first().fill(username)
+    await page.getByRole('textbox').last().fill(password)
+    await page.getByRole('button', { name: 'login' }).click()
+  }
+  const createBlog = async (page, content) => {
+    await page.getByRole('button', { name: 'create new blog' }).click()
+    await page.getByPlaceholder('blogpost title').fill(content.title)
+    await page.getByPlaceholder('blogpost author').fill(content.author)
+    await page.getByPlaceholder('blogpost url').fill(content.url);
+    await page.getByRole('button', { name: 'create' }).click()
+    await page.getByRole('button', { name: 'view' }).waitFor()
+  }
+  
+  export { loginWith, createBlog }
